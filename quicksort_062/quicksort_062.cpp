804c40bb88c951df33db04b4670df4e9a573a356
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2023-03-31 15:11:06 +0700

    void q_short

--- quicksort_062/quicksort_062.cpp
+++ quicksort_062/quicksort_062.cpp
@@ -39,3 +39,18 @@ void swap(int x, int y)
     arr[x] = arr[y];
     arr[y] = temp;
 }
+
+void q_short(int low, int high)
+{
+    int pivot, i, j;
+    if (low > high)                                             // Langkah Algoritma No.1
+        return;
+
+    // Partition the list into two parts:
+    // One containing elements less that or equal to pivot
+    // Outher containing elements greater than pivot
+
+    pivot = arr[low];                                           // Langkah Algoritma No.2
+
+    i = low + 1;                                                // Langkah Algoritma No.3
+    j = high;                                                   // Langkah Algoritma No.4
